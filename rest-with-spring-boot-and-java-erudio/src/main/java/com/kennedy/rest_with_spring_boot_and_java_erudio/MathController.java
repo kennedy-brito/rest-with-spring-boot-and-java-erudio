7935919c05fa35d44a34e0dc7a471d2607f66bcb
kennedy_brito <willian.kennedy.brito@gmail.com>
2025-01-08 14:27:11 -0300

    06 - error handling

--- rest-with-spring-boot-and-java-erudio/src/main/java/com/kennedy/rest_with_spring_boot_and_java_erudio/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/kennedy/rest_with_spring_boot_and_java_erudio/MathController.java
@@ -1,22 +1,19 @@
 package com.kennedy.rest_with_spring_boot_and_java_erudio;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class GreetingController {
+public class MathController {
 
-    private static final String template = "Hello, %s!";
     private static final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
-    public Greeting greeting(
-            @RequestParam(value = "name", defaultValue = "World")
-            String name
-    ){
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    @GetMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(@PathVariable Double numberOne, @PathVariable Double numberTwo){
+        return numberOne + numberTwo;
     }
 }
---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/kennedy/rest_with_spring_boot_and_java_erudio/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.kennedy.rest_with_spring_boot_and_java_erudio.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                request.getDescription(false), ex.getMessage(), new Date()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exceptionResponse);
+    }
+
+}
