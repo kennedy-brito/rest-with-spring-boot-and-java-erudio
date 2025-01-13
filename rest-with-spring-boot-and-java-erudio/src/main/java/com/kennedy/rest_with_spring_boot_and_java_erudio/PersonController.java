b4f8ebc0fc29affd431f40c7898892a7fa5e1dd8
kennedy_brito <willian.kennedy.brito@gmail.com>
2025-01-13 17:11:28 -0300

    13 - Content negotiation accepting return of yaml

--- rest-with-spring-boot-and-java-erudio/src/main/java/com/kennedy/rest_with_spring_boot_and_java_erudio/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/kennedy/rest_with_spring_boot_and_java_erudio/PersonController.java
@@ -3,7 +3,7 @@ package com.kennedy.rest_with_spring_boot_and_java_erudio;
 import com.kennedy.rest_with_spring_boot_and_java_erudio.data.vo.v1.PersonVO;
 import com.kennedy.rest_with_spring_boot_and_java_erudio.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import com.kennedy.rest_with_spring_boot_and_java_erudio.util.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,30 +18,30 @@ public class PersonController {
 
     @GetMapping(
             value = "/{id}",
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
     )
     public PersonVO findById(@PathVariable Long id) {
         return personService.findById(id);
     }
 
     @PostMapping(
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
     )
     public PersonVO create(@RequestBody PersonVO person) {
         return personService.create(person);
     }
 
     @PutMapping(
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
     )
     public PersonVO update(@RequestBody PersonVO person) {
         return personService.update(person);
     }
 
     @GetMapping(
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
     )
     public List<PersonVO> findAll() {
         return personService.findAll();
