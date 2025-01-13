efd5525c801561e1a0e427dec2882c060d799253
kennedy_brito <willian.kennedy.brito@gmail.com>
2025-01-13 15:21:22 -0300

    11 - migrations

---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `person` (id, address, first_name, gender, last_name) VALUES
+    (1,'Street xxxx','Willian','male','Brito'),
+    (5,'Street xxxx','Airton','male','Senna'),
+    (6,'Street xxxx','Airton','male','Senna');
+
